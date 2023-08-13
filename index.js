31c46a166e8cbf2b7907d431506fef6d01b5d743
Polichinell <tsuqinoa@gmail.com>
2023-08-13 19:53:35 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const findMatching = (matching, name) => {
+    return matching.filter(name2 => name2.toLowerCase() === name.toLocaleLowerCase())
+}
+
+const fuzzyMatch = (matching, firstLett) => {
+    return matching.filter(name => name.startsWith(firstLett))
+}
+
+const matchName = (obj, driver) => {
+    return obj.filter(person => person.name === driver)
+}
